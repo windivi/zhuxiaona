6c6665f7869251e9851d6f151d90829ae7cc7cbd
yangming54400 <yangming54400@hundsun.com>
2025-11-13 16:59:21 +0800

    update

--- scripts/private/tsc.js
+++ scripts/private/tsc.js
@@ -3,12 +3,18 @@ const Chalk = require('chalk');
 
 function compile(directory) {
   return new Promise((resolve, reject) => {
-    const tscProcess = ChildProcess.exec('tsc', {
+    // 使用 npx 来运行 tsc，确保能找到 node_modules 中的 tsc
+    const tscProcess = ChildProcess.exec('npx tsc', {
       cwd: directory,
+      maxBuffer: 10 * 1024 * 1024 // 增加 buffer 大小到 10MB
     });
 
     tscProcess.stdout.on('data', data => 
         process.stdout.write(Chalk.yellowBright(`[tsc] `) + Chalk.white(data.toString()))
+    );
+
+    tscProcess.stderr.on('data', data => 
+        process.stderr.write(Chalk.redBright(`[tsc] `) + Chalk.white(data.toString()))
     );
 
     tscProcess.on('exit', exitCode => {
@@ -17,6 +23,11 @@ function compile(directory) {
       } else {
         resolve();
       }
+    });
+
+    tscProcess.on('error', (error) => {
+      console.error(Chalk.redBright('[tsc] 执行错误:'), error.message);
+      reject(error);
     });
   });
 }
